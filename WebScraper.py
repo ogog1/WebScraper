4930ff533c7985a8c94acbcdea2e717ee5a76807
ogog1 <gorkeyomer@gmail.com>
2022-05-06 22:41:05 +0300

    new

--- WebScraper.py
+++ WebScraper.py
@@ -1,8 +1,36 @@
+import time
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
-url = "https://www.sahibinden.com/satilik/istanbul"
-page = requests.get(url)
-print(page)
+headers = {
+'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
+}
+
+
+url = "https://losangeles.craigslist.org/search/cta"
+
+posts = []
+
+for n in range(0, 5000, 120):
+    params = {
+        's':n
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code != 200:
+        print('API call failed')
+        break
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+if soup.find('pre', {'id':'moon'}:
+    break
+
+
+
+
+
+
 
 
